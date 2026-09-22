f5c8712482c2134b8753f404dd91fb0879e1d15c
Tobzi-Dev <tobzig26@gmail.com>
2026-09-22 10:52:29 +0200

    Update to 26.3 plus YACL integration for easier configuration on client

---
+++ src/main/java/com/tobzi/lobotomyinc/client/ModMenu.java
@@ -0,0 +1,13 @@
+package com.tobzi.lobotomyinc.client;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenu implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return ConfigScreen::create;
+    }
+}
+
+
